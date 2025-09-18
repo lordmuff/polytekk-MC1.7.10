22d4528c7991e44d668b69b1660620229549f0e8
mayonnaise <tranquilham@gmail.com>
2025-09-18 04:23:56 -0400

    PLEASE fix worldgen? please?

--- src/main/java/com/mayon/polytekk/PolyTekk_Server_Proxy.java
+++ src/main/java/com/mayon/polytekk/PolyTekk_Server_Proxy.java
@@ -2,9 +2,14 @@ package com.mayon.polytekk;
 
 import com.mayon.polytekk.loaders.worldgen.PT_WorldGenerator;
 import cpw.mods.fml.common.IWorldGenerator;
+import cpw.mods.fml.common.eventhandler.EventPriority;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import gregapi.api.Abstract_Proxy;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraftforge.event.terraingen.PopulateChunkEvent;
+import net.minecraftforge.event.terraingen.PopulateChunkEvent.Populate;
+
 
 import java.util.Random;
 
@@ -13,6 +18,12 @@ public class PolyTekk_Server_Proxy extends Abstract_Proxy implements IWorldGener
     @Override
     public void generate(Random aRandom, int aChunkX, int aChunkZ, World aWorld, IChunkProvider aChunkGenerator, IChunkProvider aChunkProvider) {
         PT_WorldGenerator.generate(aWorld, aChunkX << 4, aChunkZ << 4);
+    }
+
+
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    public void populate(PopulateChunkEvent.Post aEvent) {
+        PT_WorldGenerator.generate(aEvent.world, aEvent.chunkX << 4, aEvent.chunkZ << 4);
     }
 
 }
